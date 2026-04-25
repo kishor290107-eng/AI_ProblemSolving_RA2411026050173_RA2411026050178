53b353084ced50eff6caf784e0705edc662434e0
kishor290107-eng <kishor290107@gmail.com>
2026-04-25 14:26:52 +0530

    Implement A* algorithm in astar.py
    Added A* algorithm implementation with graph and heuristic.

--- Problem11/astar.py
+++ Problem11/astar.py
@@ -1,5 +1,19 @@
 import heapq
 
+graph = {
+    'A': [('B', 1), ('C', 4)],
+    'B': [('D', 2)],
+    'C': [('D', 1)],
+    'D': []
+}
+
+heuristic = {
+    'A': 7,
+    'B': 6,
+    'C': 4,
+    'D': 0
+}
+
 def astar(graph, heuristic, start, goal):
     open_list = []
     heapq.heappush(open_list, (0, start))
@@ -29,3 +43,22 @@ def astar(graph, heuristic, start, goal):
                 came_from[neighbor] = current
 
     return None
+
+
+if __name__ == "__main__":
+    start = input("Enter start node: ")
+    goal = input("Enter goal node: ")
+
+    path = astar(graph, heuristic, start, goal)
+
+    if path:
+        total_cost = 0
+        for i in range(len(path) - 1):
+            for neighbor, cost in graph[path[i]]:
+                if neighbor == path[i+1]:
+                    total_cost += cost
+
+        print("A* Path:", path)
+        print("Total Cost:", total_cost)
+    else:
+        print("No path found")
