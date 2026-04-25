86906d8d70d446f0e3239d436511e4dc966674ae
kishor290107-eng <kishor290107@gmail.com>
2026-04-25 14:25:08 +0530

    Implement BFS algorithm with user input
    Added a BFS implementation with user input for start and goal nodes.

--- Problem8/bfs.py
+++ Problem8/bfs.py
@@ -1,5 +1,12 @@
 from collections import deque
 
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D'],
+    'C': ['D'],
+    'D': []
+}
+
 def bfs(graph, start, goal):
     queue = deque([[start]])
     visited = set()
@@ -19,3 +26,15 @@ def bfs(graph, start, goal):
                 queue.append(new_path)
 
     return None
+
+
+if __name__ == "__main__":
+    start = input("Enter start node: ")
+    goal = input("Enter goal node: ")
+
+    result = bfs(graph, start, goal)
+
+    if result:
+        print("BFS Path:", result)
+    else:
+        print("No path found")
